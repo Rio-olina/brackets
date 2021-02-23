8a3867e3094e6c19b63d836eb140166949ab4362
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-23 12:29:25 +0300

    fix: change the code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = "[]{}()";
+  
+var i, l=str.length,  char,  last, stack=[];
+for(i=0; i<l; i++){
+             char=str[i];
+             if(char==="{" || char==="("){
+			 stack.push(char); last=char;}
+else if(char === '}' || char === ")"){
+                if(last){
+                     if((char === '}' && last === "{") || (char === ')' && last === '('))
+						{
+                         stack.pop(); 
+last = stack.length > 0 ? stack[stack.length - 1] : undefined;
+                     }
+                 }else{
+                     return false;
+                 }
+}  
+         }
+         return stack.length===0;
+     }
 
-let stack = [];
-let bracketsIndex = brackets.indexOf(str)
-    if(bracketsIndex % 2 === 0) {
-      stack.push(bracketsIndex + 1); 
-       return true;
-    } else {
-     
-        return false;
-      }
-    }
 
 
